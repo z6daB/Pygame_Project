f38710f3eba9968c04f6b43674bd1f98c1458be6
z6daB <patsaifedor08@yandex.ru>
2023-12-09 17:08:36 +0300

    1

---
+++ button.py
@@ -0,0 +1,17 @@
+import pygame
+from settings import *
+
+
+class Button:
+    def __init__(self):
+        self.display = pygame.display.get_surface()
+        self.font = pygame.font.Font('graphics/menu/font.ttf', 30)
+        self.surf = self.font.render('Quit', True, 'Black')
+
+    def button_quit(self):
+        button_quit = pygame.Rect(WINDOW_WIDTH / 2 - 110, WINDOW_HEIGHT / 2 + 100, 135, 50)
+        return button_quit
+
+    def button_quit_draw(self):
+        pygame.draw.rect(self.display, (102, 102, 51), self.button_quit())
+        self.display.blit(self.surf, (self.button_quit().x + 10, self.button_quit().y + 10))
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from settings import *
+from button import Button
 
 
 class Main:
@@ -9,22 +10,18 @@ class Main:
         self.clock = pygame.time.Clock()
         self.bg = pygame.image.load('graphics/menu/bg.jpg')
         self.display.blit(self.bg, (0, 0))
+        self.button = Button()
 
     def run(self):
-        font = pygame.font.Font('graphics/menu/font.ttf', 30)
-        surf = font.render('Quit', True, 'Black')
-        button_quit = pygame.Rect(WINDOW_WIDTH / 2 - 110, WINDOW_HEIGHT / 2 + 100, 135, 50)
-
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                    if button_quit.collidepoint(event.pos):
+                    if self.button.button_quit().collidepoint(event.pos):
                         running = False
-            pygame.draw.rect(self.display, (102, 102, 51), button_quit)
-            self.display.blit(surf, (button_quit.x + 10, button_quit.y + 10))
+            self.button.button_quit_draw()
             pygame.display.update()
 
 
