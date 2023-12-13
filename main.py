1b75836c7f842588760abee24e71d71034dd6e11
z6daB <patsaifedor08@yandex.ru>
2023-12-13 22:50:20 +0300

    test

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ class Main:
         pygame.init()
         self.display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         self.clock = pygame.time.Clock()
-        self.bg = pygame.image.load('graphics/menu/bg.jpg')
-        self.display.blit(self.bg, (0, 0))
         self.button = Button()
         self.select = Select()
 
@@ -20,19 +18,7 @@ class Main:
         self.button.button_settings_draw()
         self.button.button_play_draw()
         while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                    if self.button.button_quit().collidepoint(event.pos):
-                        running = False
-                    if self.button.button_settings().collidepoint(event.pos):
-                        print('settings was open')
-                    if self.button.button_play().collidepoint(event.pos):
-                        print('Playing screen')
-                        self.display.blit(self.bg, (0, 0))
-
-
+            self.select.run()
             self.clock.tick(60)
             pygame.display.update()
 
--- select_window.py
+++ select_window.py
@@ -1,8 +1,14 @@
 import pygame
+import sys
+from button import *
+from main import *
 
 class Select:
     def __init__(self):
-        self.display_surface = pygame.display.get_surface()
+        self.display = pygame.display.get_surface()
+        self.bg = pygame.image.load('graphics/menu/bg.jpg')
+        self.display.blit(self.bg, (0, 0))
+        self.button = Button()
 
     def run(self):
         self.event_loop()
@@ -11,9 +17,21 @@ class Select:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.QUIT
+                sys.exit()
             self.check_buttons(event)
 
     def check_buttons(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             if self.button.button_quit().collidepoint(event.pos):
                 pygame.QUIT
+                sys.exit()
+            if self.button.button_settings().collidepoint(event.pos):
+                print('settings was open')
+            if self.button.button_play().collidepoint(event.pos):
+                self.playing_screen()
+
+
+
+    def playing_screen(self):
+        print('Playing screen')
+        self.display.blit(self.bg, (0, 0))
