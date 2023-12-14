c87c5267f5e3f19aea9667278711bd85b2e5b89f
z6daB <patsaifedor08@yandex.ru>
2023-12-14 16:44:10 +0300

    draw characters

--- main.py
+++ main.py
@@ -9,15 +9,10 @@ class Main:
         pygame.init()
         self.display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         self.clock = pygame.time.Clock()
-        self.button = Button()
         self.select = Select()
 
     def run(self):
-        running = True
-        self.button.button_quit_draw()
-        self.button.button_settings_draw()
-        self.button.button_play_draw()
-        while running:
+        while True:
             self.select.run()
             self.clock.tick(60)
             pygame.display.flip()
--- select_window.py
+++ select_window.py
@@ -9,6 +9,9 @@ class Select:
         self.bg = pygame.image.load('graphics/menu/bg.jpg')
         self.display.blit(self.bg, (0, 0))
         self.button = Button()
+        self.button.button_quit_draw()
+        self.button.button_settings_draw()
+        self.button.button_play_draw()
 
     def run(self):
         self.event_loop()
@@ -26,14 +29,18 @@ class Select:
                 pygame.QUIT
                 sys.exit()
             if self.button.button_settings().collidepoint(event.pos):
+                self.button.button_settings()
                 print('settings was open')
             if self.button.button_play().collidepoint(event.pos):
-                self.hero_choice()
+                self.draw_characters()
 
 
 
-    def hero_choice(self):
-        print('Playing screen')
-        black_human = pygame.image.load('graphics/menu/characters/character_malePerson_behindBack.png')
+    def draw_characters(self):
+        black_man = pygame.image.load('graphics/menu/characters/character_malePerson_behindBack.png')
+        woman = pygame.image.load('graphics/menu/characters/woman.png')
+        white_man = pygame.image.load('graphics/menu/characters/white_man.png')
         self.display.blit(self.bg, (0, 0))
-        self.display.blit(black_human, (150, 150))
+        self.display.blit(black_man, (150, 150))
+        self.display.blit(woman, (550, 150))
+        self.display.blit(white_man, (950, 150))
