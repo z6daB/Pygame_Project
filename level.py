00a4b7419d7a5e65f14553b488ae362c395c4a28
z6daB <patsaifedor08@yandex.ru>
2023-12-29 20:48:51 +0300

    camera

--- level.py
+++ level.py
@@ -1,20 +1,43 @@
 import pygame
 from settings import *
 from player import Player
-
+from tile import Tile
 
 class Level:
     def __init__(self):
         self.display = pygame.display.get_surface()
         # sprite groups
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = CameraGroup()
         self.invisible_sprites = pygame.sprite.Group()
 
         self.create_player()
 
     def create_player(self):
-        Player((100, 100), self.visible_sprites)
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x, y), [self.visible_sprites, self.invisible_sprites])
+                if col == 'p':
+                    self.player = Player((100, 100), self.visible_sprites, self.invisible_sprites)
 
     def run(self):
-        self.visible_sprites.draw(self.display)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
+
+
+class CameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display = pygame.display.get_surface()
+        self.half_width = self.display.get_size()[0] // 2
+        self.half_height = self.display.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+
+    def custom_draw(self, player):
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display.blit(sprite.image, offset_pos)
--- player.py
+++ player.py
@@ -2,33 +2,57 @@ import pygame
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, *groups):
-        super().__init__(*groups)
+    def __init__(self, pos, groups, invisible_sprites):
+        super().__init__(groups)
         self.image = pygame.image.load('graphics/characters/black_character/soldier_walk1.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
         self.direction = pygame.math.Vector2()
         self.speed = 5
 
+        self.invisible_sprites = invisible_sprites
+
     def keyboard_buttons(self):
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_w] or keys[pygame.K_UP]:
+        if keys[pygame.K_UP]:
             self.direction.y = -1
-        elif keys[pygame.K_s] or keys[pygame.K_DOWN]:
+        elif keys[pygame.K_DOWN]:
             self.direction.y = 1
         else:
             self.direction.y = 0
 
-        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+        if keys[pygame.K_LEFT]:
             self.direction.x = -1
-        elif keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+        elif keys[pygame.K_RIGHT]:
             self.direction.x = 1
         else:
             self.direction.x = 0
 
-    def move(self):
-        self.rect.center += self.direction * self.speed
+    def move(self, speed):
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+        self.rect.x += self.direction.x * speed
+        self.collision('horizontal')
+        self.rect.y += self.direction.y * speed
+        self.collision('vertical')
+
+    def collision(self, direction):
+        if direction == 'horizontal':
+            for sprite in self.invisible_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.x > 0:
+                        self.rect.right = sprite.rect.left
+                    if self.direction.x < 0:
+                        self.rect.left = sprite.rect.right
+
+        if direction == 'vertical':
+            for sprite in self.invisible_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.y > 0:
+                        self.rect.bottom = sprite.rect.top
+                    if self.direction.y < 0:
+                        self.rect.top = sprite.rect.bottom
 
     def update(self):
         self.keyboard_buttons()
-        self.move()
+        self.move(self.speed)
