b5882543ca803a88b655ec73e152ae5b0bcb8d3a
z6daB <patsaifedor08@yandex.ru>
2024-01-04 00:22:14 +0300

    changes once every five minutes in the amount of radiation, water and health

--- interface.py
+++ interface.py
@@ -9,29 +9,50 @@ class Interface:
         self.display = pygame.display.get_surface()
 
         self.hp_value = 100
-        self.water_value = 70
-        self.radiation_value = 20
+        self.water_value = 100
+        self.radiation_value = 0
 
         self.start_ticks = pygame.time.get_ticks()
         self.tick_interval = 5000
 
     def draw_bars(self):
         # draw bg
-        pygame.draw.rect(self.display, (63, 64, 62), (20, 20, HEALTH_WIDTH * 100, HEALTH_HEIGHT))
-        pygame.draw.rect(self.display, (63, 64, 62), (20, 50, WATER_WIDTH * 100, WATER_HEIGHT))
-        pygame.draw.rect(self.display, (63, 64, 62), (20, 80, RADIATION_WIDTH * 100, RADIATION_HEIGHT))
+        pygame.draw.rect(
+            self.display, (63, 64, 62), (20, 20, 200, HEALTH_HEIGHT)
+        )
+        pygame.draw.rect(
+            self.display, (63, 64, 62), (20, 50, WATER_WIDTH * 100, WATER_HEIGHT)
+        )
+        pygame.draw.rect(
+            self.display, (63, 64, 62), (20, 80, RADIATION_WIDTH * 100, RADIATION_HEIGHT)
+        )
 
-        pygame.draw.rect(self.display, (255, 0, 0), (20, 20, HEALTH_WIDTH * self.hp_value, HEALTH_HEIGHT))
-        pygame.draw.rect(self.display, (0, 0, 255), (20, 50, WATER_WIDTH * self.water_value, WATER_HEIGHT))
-        pygame.draw.rect(self.display, (42, 255, 4), (20, 80, RADIATION_WIDTH * self.radiation_value, RADIATION_HEIGHT))
+        # draw real bars
+        pygame.draw.rect(
+            self.display, (255, 0, 0), (20, 20, HEALTH_WIDTH * self.hp_value, HEALTH_HEIGHT)
+        )
+        pygame.draw.rect(
+            self.display, (0, 0, 255), (20, 50, WATER_WIDTH * self.water_value, WATER_HEIGHT)
+        )
+        pygame.draw.rect(
+            self.display, (42, 255, 4), (20, 80, RADIATION_WIDTH * self.radiation_value, RADIATION_HEIGHT)
+        )
 
     def update(self):
         current_ticks = pygame.time.get_ticks()
         if self.hp_value <= 0:
             self.hp_value = 100
             ChangeScreen('dead')
-            print('dead')
         else:
             if current_ticks - self.start_ticks > self.tick_interval:
-                self.hp_value -= 5
+                if self.water_value > 0:
+                    self.water_value -= 1
+                elif self.water_value <= 0:
+                    self.hp_value -= 1
+
+                if self.radiation_value < 100:
+                    self.radiation_value += 5
+                elif self.radiation_value == 100:
+                    self.hp_value -= 1
+
                 self.start_ticks = current_ticks
--- player.py
+++ player.py
@@ -8,7 +8,7 @@ class Player(pygame.sprite.Sprite):
         self.display = pygame.display.get_surface()
         self.image = pygame.image.load('graphics/characters/black_character/soldier_walk1.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
-        self.hitbox = self.rect.inflate(0, 0)
+        self.hitbox = self.rect.inflate(0, -28)
 
         # создание переменной, отвечающей за направление
         self.direction = pygame.math.Vector2()
