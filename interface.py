fb6337dfe51cd2fb41e1d580be70ba8e700abb22
z6daB <patsaifedor08@yandex.ru>
2024-01-28 21:49:19 +0300

    edit

--- interface.py
+++ interface.py
@@ -44,27 +44,30 @@ class Interface:
 
         # draw real bars
         pygame.draw.rect(
-            self.display, (255, 0, 0), (20, 600, HEALTH_WIDTH * min(100, self.game.level.player.hp_value), HEALTH_HEIGHT)
+            self.display, (255, 0, 0),
+            (20, 600, HEALTH_WIDTH * min(100, self.game.level.player.hp_value), HEALTH_HEIGHT)
         )
         pygame.draw.rect(
-            self.display, (0, 0, 255), (20, 630, WATER_WIDTH * min(100,self.game.level.player.water_value), WATER_HEIGHT)
+            self.display, (0, 0, 255),
+            (20, 630, WATER_WIDTH * min(100, self.game.level.player.water_value), WATER_HEIGHT)
         )
         pygame.draw.rect(
-            self.display, (42, 255, 4), (20, 660, RADIATION_WIDTH * min(100,self.game.level.player.radiation_value), RADIATION_HEIGHT)
+            self.display, (42, 255, 4),
+            (20, 660, RADIATION_WIDTH * max(0, self.game.level.player.radiation_value), RADIATION_HEIGHT)
         )
         pygame.draw.rect(
             self.display, (255, 222, 0), (400, 45, 100 * self.memories_value, 55)
         )
         # count
         self.drawer.drawer_text(12, f'{min(100, self.game.level.player.hp_value)} / 100', 'White', (75, 605))
-        self.drawer.drawer_text(12, f'{min(100,self.game.level.player.water_value)} / 100', 'White', (75, 635))
-        self.drawer.drawer_text(12, f'{min(100,self.game.level.player.radiation_value)} / 100', 'White', (75, 665))
+        self.drawer.drawer_text(12, f'{min(100, self.game.level.player.water_value)} / 100', 'White', (75, 635))
+        self.drawer.drawer_text(12, f'{max(0, self.game.level.player.radiation_value)} / 100', 'White', (75, 665))
         self.drawer.drawer_text(24, f'{self.memories_value} / 5', 'White', (575, 60))
 
         # inventory
-        pygame.draw.circle(self.display, (131, 131, 89), (WINDOW_WIDTH-60, 60), 40)
+        pygame.draw.circle(self.display, (131, 131, 89), (WINDOW_WIDTH - 60, 60), 40)
         self.inventory = pygame.image.load('map/inventory.png')
-        self.display.blit(self.inventory, (WINDOW_WIDTH-92, 25))
+        self.display.blit(self.inventory, (WINDOW_WIDTH - 92, 25))
 
         if self.button.inventory_button().collidepoint(pygame.mouse.get_pos()):
             self.button.inventory_button_draw_hover()
@@ -99,4 +102,3 @@ class Interface:
         weapon_index = self.game.level.player.weapon_index
         pygame.draw.rect(self.display, (131, 131, 89), (WINDOW_WIDTH - 250, WINDOW_HEIGHT - 120, 256, 128))
         self.display.blit(weapon_images[weapon_index], (WINDOW_WIDTH - 250, WINDOW_HEIGHT - 115))
-
