6d05f9b3fc51a469bd3e4b826051f532b9214c5b
z6daB <patsaifedor08@yandex.ru>
2024-01-29 14:46:42 +0300

    edit

--- item.py
+++ item.py
@@ -27,7 +27,7 @@ class Item:
 
         self.formula_craft = {
             'pill_health': [['medicinal_substances', 6]],
-            'gasmaskAll': [['iron', 1], ['gasmask', 1]],
+            'gasmaskAll': [['iron', 4], ['gasmask', 1]],
             'pill_water': [['kumquat', 5]],
             'glock': [['iron', 8]],
             'ak47': [['iron', 13], ['wood', 7]],
